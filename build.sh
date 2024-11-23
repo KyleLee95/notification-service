28c69367ae91be03bb6b50f006b9d41b2d0b5cc1
Kyle Lee <kyle@kylelee.dev>
2024-11-23 15:47:24 -0500

    setup prisma

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker run -d -e POSTGRES_DB=auction-service-db \
+docker run -d -e POSTGRES_DB=notification-service-db \
 	-e POSTGRES_PASSWORD=password \
 	-e POSTGRES_USER=postgres \
-	-p "8080:5432" postgres
+	-p "8081:5432" postgres
