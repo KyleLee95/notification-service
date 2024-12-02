a8f734ab75ed54ea5acb5abfb71afa6a36d2e9bf
Kyle Lee <kyle@kylelee.dev>
2024-12-02 11:35:49 -0500

    add mq consumer + pub + routing keys

--- src/routes/mq.ts
+++ src/routes/mq.ts
@@ -1,16 +1,13 @@
 import { type FastifyInstance } from "fastify";
-//
-// export const messageQueueRoutes = async (router: FastifyInstance) => {
-//   router.post("/send-message", async (request, reply) => {
-//     const { userId, adminId, content } = request.body as {
-//       userId: number;
-//       adminId: number;
-//       content: string;
-//     };
-//
-//     // Publish message to RabbitMQ
-//     await publishMessage("support-messages", { userId, adminId, content });
-//
-//     return { success: true };
-//   });
-// };
+
+export const messageQueueRoutes = async (router: FastifyInstance) => {
+  router.post("/activate-auction", async (request, reply) => {
+    const { userId, adminId, content } = request.body as {
+      userId: number;
+      adminId: number;
+      content: string;
+    };
+
+    return { success: true };
+  });
+};
