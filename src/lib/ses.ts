64cd97422098e642a049214f07a1ff02bfe76654
Kyle Lee <kyle@kylelee.dev>
2024-12-10 13:16:54 -0600

    remove ! from keys

--- src/lib/ses.ts
+++ src/lib/ses.ts
@@ -5,8 +5,8 @@ const sesClient = new SESClient([
   {
     region: "us-east-2",
     credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID!,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID,
     },
   },
 ]);
